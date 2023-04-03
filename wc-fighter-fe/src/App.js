6b9a5e8cf6ed1fb97221521e2a0d4a1f7213538b
MWeberLambdaweb19 <mack.webb37@gmail.com>
2023-04-03 15:39:49 -0700

    Rewriting logic for boxing program

--- wc-fighter-fe/src/App.js
+++ wc-fighter-fe/src/App.js
@@ -10,10 +10,19 @@ function SampleProgram() {
     5: document.getElementById('Gate-Lower-Right')
   }
   let sessionMoves = 10
-  let currentMoves = 0
-  let gateNum = Math.floor(Math.random() * 6)
-  let gatePick = gates[gateNum].classList;
-  gatePick.add('Selected')
+  for (let i = 0; i < sessionMoves; i++) {
+    console.log('Start')
+    let gateNum = Math.floor(Math.random() * 6)
+    let gatePick = gates[gateNum].classList;
+    console.log('Gate picked', gatePick);
+    gatePick.add('Selected')
+    setTimeout(() => {
+      console.log('Gate modified', gatePick)
+      gatePick.remove('Selected')
+    }, 3000)
+    console.log('Gate reset', gatePick)
+    console.log('Program ended')
+  }
 }
 
 function App() {
@@ -22,17 +31,15 @@ function App() {
       <button onClick={SampleProgram}>Run Test</button>
       <div className='Fighter-Box'>
         {/* Upper Gates */}
-        <div id='Gate-Upper-Left' className='Gate'>Upper Left</div>
-        <div id='Gate-Upper-Right' className='Gate'>Upper Right</div>
+        <div id='Gate-Upper-Left' className='Gate Gate-Upper-Left'>Upper Left</div>
+        <div id='Gate-Upper-Right' className='Gate Gate-Upper-Right'>Upper Right</div>
         {/* Middle Gates */}
-        <div id='Gate-Middle-Left' className='Gate'>Middle Left</div>
-        <div id='Gate-Middle-Right' className='Gate'>Middle Right</div>
+        <div id='Gate-Middle-Left' className='Gate Gate-Middle-Left'>Middle Left</div>
+        <div id='Gate-Middle-Right' className='Gate Gate-Middle-Right'>Middle Right</div>
         {/* Lower Gates */}
-        <div id='Gate-Lower-Left' className='Gate'>Lower Left</div>
-        <div id='Gate-Lower-Right' className='Gate'>Lower Right</div>  
+        <div id='Gate-Lower-Left' className='Gate Gate-Lower-Left'>Lower Left</div>
+        <div id='Gate-Lower-Right' className='Gate Gate-Lower-Right'>Lower Right</div>  
       </div>
-      <footer className='App-header'>
-      </footer>
     </div>
   );
 }

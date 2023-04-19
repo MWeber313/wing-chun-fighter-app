25f86b60f63923b63e5895f3b6bbce67b0029525
MWeberLambdaweb19 <mack.webb37@gmail.com>
2023-04-19 10:24:46 -0700

    function runs!

--- wc-fighter-fe/src/App.js
+++ wc-fighter-fe/src/App.js
@@ -22,58 +22,86 @@ function App () {
         }
     ]
 
-
-
     async function CircuitPunching() {
-        let moves = document.getElementById('moves').value
-        let speed = (document.getElementById('speed').value * 1000)
-        // let gatePick = gates[Math.floor(Math.random() * gates.length)]
-        // console.log(gatePick)
-        // let divPick = document.getElementById(gatePick.name)
-        // console.log(divPick)
+        // How many moves per program run
+        let moves = 0
+        if (document.getElementById('moves').value == '') {
+            moves = 10
+        }
+        else {
+            moves = document.getElementById('moves').value
+        }
 
-        // Is this coherent and easy to read?
+        // Speed in whole numbers, will be multiplied by 1000ms
+        let speed = 0
+        if (document.getElementById('speed').value == '') {
+            speed = 3
+        }
+        else {
+            speed = document.getElementById('speed').value
+        } 
+
+        console.log(moves)
+        console.log(speed)
+
         let prevPick = undefined
-
-        function gatePick() {
-            let divPick = document.getElementById(gates[Math.floor(Math.random() * gates.length)].name)
-
-            if (divPick == prevPick) {
-                gatePick()
+        
+        // Is this coherent and easy to read?
+        function gateReRoll(divPick) {
+            if (prevPick == undefined) {
+                console.log('Continuing')
+                return
             }
-            else {
+            else if (divPick == prevPick) {
+                console.log('Match!')
+                console.log('Previous: ', prevPick)
+                console.log('Current: ', divPick)
+                let divPick = document.getElementById(gates[Math.floor(Math.random() * gates.length)].name)
+                console.log('Updated: ', divPick)
                 return divPick
             }
         }
 
-        for (let i = 0; i < moves; i++) {
-
-            let divPick = gatePick()
+        for (let i = 0; i <= moves; i++) {
+            let divPick = document.getElementById(gates[Math.floor(Math.random() * gates.length)].name)
             
+            gateReRoll(divPick)
             divPick.classList.add('Selected')
     
             await new Promise((resolve, reject) => {
-                setTimeout(resolve, speed)
+                setTimeout(resolve, (speed * 1000))
             })
             
             divPick.classList.remove('Selected')
-            prevPick = divPick
+            // prevPick = divPick
         }
+
+        // let gatePick = gates[Math.floor(Math.random() * gates.length)]
+
+        // let divPick = document.getElementById(gatePick.name)
+        // console.log(divPick)
+        // divPick.classList.add('Selected')
+
+        // await new Promise((resolve, reject) => {
+        //     setTimeout(resolve, (speed*1000))
+        // })
+
+        // divPick.classList.remove('Selected')
 
     }
 
     return (
         <div className="App">
             <div className="Setup-Box">
-                <label for="moves">Moves</label>
-                <input type="number" name="moves"></input>
-                <label for="speed">Speed</label>
-                <input type="number" name="speed"></input>
-                <button onClick={CircuitPunching}>Run Program</button>
+                <label htmlFor="moves">Moves</label>
+                <input id="moves" type="number" name="moves"></input>
+                <label htmlFor="speed">Speed</label>
+                <input id="speed" type="number" name="speed"></input>
+                <button onClick={()=>CircuitPunching()}>Run Program</button>
             </div>
             <div className="Fighter-Box">
                 {gates.map((item) => 
-                <div key={item.index} id={item.name} className="Gate">{item.name}</div>
+                <div key={item.name} id={item.name} className="Gate">{item.name}</div>
                 )}
             </div>
         </div>
